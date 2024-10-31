789f618cf95409417b575c5eab8da89c25f4912c
InvizHer <112791490+InvizHer@users.noreply.github.com>
2024-10-31 19:21:43 +0530

    Update postRoutes.js

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,8 +1,14 @@
 const express = require('express');
-const { getAllPosts, createPost, editPost, deletePost } = require('../controllers/postController');
 const router = express.Router();
-router.get('/', getAllPosts);
-router.post('/', createPost);
-router.put('/:id', editPost);
-router.delete('/:id', deletePost);
+const postController = require('../controllers/postController');
+const path = require('path');
+router.get('/admin/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views/login.html'));
+});
+router.get('/admin/dashboard', postController.adminDashboard);
+router.get('/admin/edit/:id', postController.editPostPage);
+router.post('/admin/create', postController.createPost);
+router.post('/admin/edit/:id', postController.updatePost);
+router.post('/admin/delete/:id', postController.deletePost);
+router.get('/', postController.getAllPosts);
 module.exports = router;
