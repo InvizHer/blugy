61f5402f6f11195a63f3be3f6d860947f6eebf55
InvizHer <112791490+InvizHer@users.noreply.github.com>
2024-10-31 19:23:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,28 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const postRoutes = require('./routes/postRoutes');
 const dotenv = require('dotenv');
-const path = require('path');
-
-dotenv.config();
 const connectDB = require('./config/db');
-
 const app = express();
+dotenv.config();
 connectDB();
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // To parse URL-encoded data
-
-app.use(express.static(path.join(__dirname, '../frontend'))); // Serve static files from the frontend directory
-
-app.use('/api/posts', postRoutes); // API routes for posts
-
-// Serve the index.html file at the root URL
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/index.html'));
-});
-
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api/posts', postRoutes);
+app.listen(process.env.PORT || 5000, () => {
+    console.log('Server is running');
 });
